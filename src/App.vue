f1238e4947b8572aed30ea78184019295148969e
sergiopalacio <sergio.palaciorodenas@ingrammicro.com>
2020-01-23 13:02:19 +0100

    package.json modifications

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <div id="app" style="text-align: center; padding: 10vw; max-width: 960px; width: 90vw">
-    <div>
-      <!-- <NumberInputSpinner /> -->
-      <hr>
-      <NumberInputSpinner :integerOnly="false"  value='10' :max="1000" v-model="test"/>
+    <div id="app" style="text-align: center; padding: 10vw; max-width: 960px; width: 90vw">
+        <div>
+            <!-- <NumberInputSpinner /> -->
+            <hr>
+            <number-input-spinner :integerOnly="false" value='10' :max="1000" v-model="row.value"/>
+
+            <input type="number" v-model="row.value">
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import NumberInputSpinner from 'components/NumberInputSpinner.vue';
+  import NumberInputSpinner from 'components/NumberInputSpinner.vue';
 
-export default {
+  export default {
     components: {
-        NumberInputSpinner,
+      NumberInputSpinner,
     },
 
     data: function () {
-        return {
-            test: 10,
-        };
+      return {
+        row: {
+          value: 10
+        },
+      };
     },
-};
+  };
 </script>
